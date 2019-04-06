67117d9d99de7bee636186fc5adf04222f084bb0
neaj mahmud <neajmahmud230@gmail.com>
2019-04-05 22:28:30 -0500

    done

--- practice.js
+++ practice.js
@@ -28,142 +28,186 @@
 */
 
 // Code Here 
-
-// Do not edit the code below.
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-
-first(names, function(firstName){
-  console.log('The first name in names is ' + firstName);
-  return firstName;
-});
-// Do not edit the code above.
-
-
-
-////////// PROBLEM 2 //////////
-
-/*
-  Write a function called last that takes in an array and a callback function. 
-  Then invoke the callback, passing in the last element in the array as the argument.
-*/
-
-//Code Here
-
-// Do not edit the code below.
-last(names, function(lastName){
-  console.log('The last name in names is ' + lastName);
-  return lastName;
-});
-// Do not edit the code above.
-
-
-
-////////// PROBLEM 3 //////////
-
-/*
-  Write a function called multiply that takes in three parameters: two numbers and a callback function.  
-  Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
-*/
-
-//Code Here
-
-// Do not edit the code below.
-multiply(4, 3, function(answer){
-  console.log('The answer is ' + answer); //should console.log 12
-});
-// Do not edit the code above.
-
-
-
-////////// PROBLEM 4 //////////
-
-/*
-  Write a function called contains that takes in three parameters: an array, a name and a callback.  
-  Check if the name exists in the array. 
-  If it does, invoke the callback with true as the argument. 
-  If the name does not exist, invoke the callback with false as the argument.
-*/
-
-//Code Here 
-
-// Do not edit the code below.
-contains(names, 'Colt', function(result){
-  if(result === true){
-    console.log('Colt is in the array');
-  } else {
-    console.log('Colt is not in the array');
+function first (array , callback){
+  callback(array[0]) ;
+  };
+  
+  
+  
+  // Do not edit the code below.
+  var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+  
+  first(names, function(firstName){
+    console.log('The first name in names is ' + firstName);
+    return firstName;
+  });
+  // Do not edit the code above.
+  
+  
+  
+  ////////// PROBLEM 2 //////////
+  
+  /*
+    Write a function called last that takes in an array and a callback function. 
+    Then invoke the callback, passing in the last element in the array as the argument.
+  */
+  
+  //Code Here
+  function last(array,callback){
+  callback(array[array.length -1]);
+  };
+  
+  
+  
+  // Do not edit the code below.
+  last(names, function(lastName){
+    console.log('The last name in names is ' + lastName);
+    return lastName;
+  });
+  // Do not edit the code above.
+  
+  
+  
+  ////////// PROBLEM 3 //////////
+  
+  /*
+    Write a function called multiply that takes in three parameters: two numbers and a callback function.  
+    Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
+  */
+  
+  //Code Here
+  function multiply(num1,num2,callback){
+    
+    callback(num1 * num2)
+  
+  };
+  // Do not edit the code below.
+  multiply(4, 3, function(answer){
+    console.log('The answer is ' + answer); //should console.log 12
+  });
+  // Do not edit the code above.
+  
+  
+  
+  ////////// PROBLEM 4 //////////
+  
+  /*
+    Write a function called contains that takes in three parameters: an array, a name and a callback.  
+    Check if the name exists in the array. 
+    If it does, invoke the callback with true as the argument. 
+    If the name does not exist, invoke the callback with false as the argument.
+  */
+  
+  
+  
+  
+  //Code Here 
+  function contains(arr,name,callback){
+    if(arr.includes(name)){
+     callback(true);
+    }else{
+     callback(false);
+    }
+    
   }
-});
-// Do not edit the code above.
-
-
-
-////////// PROBLEM 5 //////////
-
-/*
-  Write a function called uniq that takes in an array and a callback function.
-  Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
-*/
-
-//Code Here
-
-// Do not edit the code below.
-uniq(names, function(uniqArr){
-  console.log('The new names array with all the duplicate items removed is ', uniqArr);
-});
-// Do not edit the code above.
-
-
-
-////////// PROBLEM 6 //////////
-
-/* 
-  Write a function called each that takes in an array of names and a callback function. 
-  For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
-*/
-
-//Code Here 
-
-// Do not edit the code below.
-each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
-});
-// Do not edit the code above.
-
-
-
-////////// PROBLEM 7 //////////
-
-/*
-  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
-  When the correct user object is found, invoke the callback with the user object as an argument.
-*/
-
-// Code here
-
-// Do not edit the code below.
-var users = [
-  {
-    id: '12d',
-    email: 'tyler@gmail.com',
-    name: 'Tyler',
-    address: '167 East 500 North'
-  },
-  {
-    id: '15a',
-    email: 'cahlan@gmail.com',
-    name: 'Cahlan',
-    address: '135 East 320 North'
-  },
-  {
-    id: '16t',
-    email: 'ryan@gmail.com',
-    name: 'Ryan',
-    address: '192 East 32 North'
-  },
-];
-
-getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
-});
-// Do not edit the code above.
+  // Do not edit the code below.
+  contains(names, 'Colt', function(result){
+    if(result === true){
+      console.log('Colt is in the array');
+    } else {
+      console.log('Colt is not in the array');
+    }
+  });
+  // Do not edit the code above.
+  
+  
+  
+  ////////// PROBLEM 5 //////////
+  
+  /*
+    Write a function called uniq that takes in an array and a callback function.
+    Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
+  */
+  
+  //Code Here
+  
+  // Do not edit the code below.
+  uniq(names, function(uniqArr){
+    console.log('The new names array with all the duplicate items removed is ', uniqArr);
+  });
+  // Do not edit the code above.
+  
+  function uniq(names, callback) {
+    for (let i = 0; i < names.length; i++) {
+      for (let k = i + 1; k < names.length; k++) {
+        if (names[i] === names[k]) {
+          names.splice(k, 1)
+        }
+      }
+    }
+    callback(names);
+  }
+  
+  
+  ////////// PROBLEM 6 //////////
+  
+  /* 
+    Write a function called each that takes in an array of names and a callback function. 
+    For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
+  */
+  
+  //Code Here 
+  function each(arr,callback){
+    for(var i = 0; i < arr.length; i++){
+       callback(arr[i], i)
+    }
+  }
+  // Do not edit the code below.
+  each(names, function(item, indice){
+    console.log('The item in the ' + indice + ' position is ' + item)
+  });
+  // Do not edit the code above.
+  
+  
+  
+  ////////// PROBLEM 7 //////////
+  
+  /*
+    Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
+    When the correct user object is found, invoke the callback with the user object as an argument.
+  */
+  
+  // Code here
+   function getUserById(arr, id, callback){
+    for(let i = 0; i < arr.length; i++){
+      if (id === arr[i].id){
+        return callback(arr[i])
+      }
+    }
+   }
+  // Do not edit the code below.
+  var users = [
+    {
+      id: '12d',
+      email: 'tyler@gmail.com',
+      name: 'Tyler',
+      address: '167 East 500 North'
+    },
+    {
+      id: '15a',
+      email: 'cahlan@gmail.com',
+      name: 'Cahlan',
+      address: '135 East 320 North'
+    },
+    {
+      id: '16t',
+      email: 'ryan@gmail.com',
+      name: 'Ryan',
+      address: '192 East 32 North'
+    },
+  ];
+  
+  getUserById(users, '16t', function(user){
+    console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+  });
+  // Do not edit the code above.
